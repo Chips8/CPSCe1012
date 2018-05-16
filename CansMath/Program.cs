d85549a09ddd38b9400a520c1954b8b59eb0ea13
Chips8 <Clarencekfwong@hotmail.com>
2018-05-16 17:32:21 -0600

    Add files via upload

---
+++ CansMath/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CansMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfCans = 72;
+            decimal pricePerCan = 0.40m;
+            double discountPercentage = 0.15;
+            decimal discountPricePerCan = pricePerCan - (pricePerCan * (decimal)discountPercentage);
+
+            decimal totalCost = discountPricePerCan * numOfCans;
+            Console.WriteLine($"The price for {numOfCans} is {totalCost:c}");
+            Console.WriteLine("Price for {0} cans is ${1:0.000}", numOfCans, totalCost);
+            Console.ReadLine();
+        }
+    }
+}
