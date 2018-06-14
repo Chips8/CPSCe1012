15a131ce5c478ef517f800474dae581441115892
Chips8 <Clarencekfwong@hotmail.com>
2018-06-13 18:36:30 -0600

    Add files via upload

---
+++ June 13, 2018/Special Random Example/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Random_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                //Random rnd = new Random(); Will cause inconsistent numbers because it is using the same time stamp to generate a number.
+                Console.WriteLine(rnd.Next(1, 101));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
