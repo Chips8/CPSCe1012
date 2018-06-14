c319b486dedb297a08a5ed52eb25b207e4593791
Chips8 <Clarencekfwong@hotmail.com>
2018-06-13 18:32:19 -0600

    Add files via upload

---
+++ June 13, 2018/Loop Problems 2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loop_Problems_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 0, generated = 0, guessCount = 0;
+
+            Random rnd = new Random();
+            bool winner = false;
+            generated = rnd.Next(1, 101);
+
+            do
+            {
+                Console.Write("Enter your guess from 1 to 100");
+                input = int.Parse(Console.ReadLine());
+                guessCount++;
+
+                if(generated == input)
+                {
+                    Console.WriteLine("You Win");
+                    winner = true;
+
+                }
+                else
+                {
+                    if (input < generated)
+                    {
+                        Console.WriteLine("Too Low");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Too High");
+                    }
+                }
+
+
+
+
+            } while (winner == false);
+            Console.WriteLine($"You win, it took {guessCount} tries!");
+            Console.ReadKey();
+        }
+    }
+}
