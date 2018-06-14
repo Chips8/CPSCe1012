14d6d39ac74339559ede6116b01a7b8a2e9e044b
Chips8 <Clarencekfwong@hotmail.com>
2018-06-13 18:49:35 -0600

    Add files via upload

---
+++ June 13, 2018/TriangleEmpty/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleEmpty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = 5;
+
+            for (int i = 1; i <= input; i++)
+            {
+                int numberOfSpace = input - i;
+                for (int j = 0; j < numberOfSpace*2; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 1; j < i * 2; j++)
+                {
+                    //Remove if to get filled triangle.
+                    if (j == 1 || j == (i * 2 - 1) || i == input)
+                    {
+                        Console.Write("* ");
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                    //Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
